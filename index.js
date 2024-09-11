fa19caf89555e59deb64b5f169f884805fdae26f
Kayran048 <kayranxav@gmail.com>
2024-09-10 23:39:14 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 
 let meta = {
@@ -9,6 +9,8 @@ let meta = {
 
 let metas = [ meta ]
 
+
+//Cadastrar meta
 const cadastrarMeta = async () => {
   const meta = await input({ message : "Digite a meta: "})
   
@@ -22,6 +24,35 @@ const cadastrarMeta = async () => {
   )
 }
 
+//Listar metas
+const listarMetas = async () => {
+  const respostas = await checkbox({
+    message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+    choices: [...metas],
+    instructions: false,
+  })
+
+  if (respostas.length == 0){
+    console.log("Nenhuma meta selecionada")
+    return
+  }
+
+
+  metas.forEach((m) =>{
+    m.checked = false
+  })
+
+
+  respostas.forEach((resposta) => {
+    const meta = metas.find((m) => {
+      return m.value == resposta
+    })
+
+    meta.checked = true
+  })
+
+  console.log("Meta(s) marcadas como concluída(s).")
+}
 
 
 
@@ -56,7 +87,7 @@ const start = async() => {
         console.log(metas)
         break
       case "listar":
-        console.log("Vamos listar")
+        await listarMetas()
         break
       case "sair":
         console.log("Até a próxima!")
